6adcd8c1c94e1ab51b6807012ee6f08b131c11e6
allison ♡ <75504637+allcre@users.noreply.github.com>
2023-05-28 01:07:41 -0400

    used ternary to make total red if 0% tip

---
+++ Project 1 - WeSplit/WeSplit-v3.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Allison on 2023-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var chequeAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 15
+    @FocusState private var amountIsFocused: Bool
+     
+    var currency: FloatingPointFormatStyle<Double>.Currency {
+        .currency(code: Locale.current.currency?.identifier ?? "CAD")
+    }
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        return (tipSelection / 100 + 1) * chequeAmount
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $chequeAmount, format: currency)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                } header: {
+                    Text("Total cheque amount")
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(grandTotal, format: currency)
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
+                } header: {
+                    Text("Total Cost")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currency)
+                } header: {
+                    Text("Cost per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
